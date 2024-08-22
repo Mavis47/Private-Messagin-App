02b3ddd073e516dacfe540d954bd73dcf2b503cb
manish vishwakarma <prakashvish1973@gmail.com>
2024-08-22 11:46:00 +0530

    cors update location

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,11 +7,12 @@ import cors from "cors";
 
 const PORT = process.env.PORT || 5001
 
+
+app.use(express.json()); //for parsing json data
 app.use(cors({
     origin: process.env.ALLOWED_ORIGIN,
     methods: ['GET','POST'],
 }))
-app.use(express.json()); //for parsing json data
 app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes);
